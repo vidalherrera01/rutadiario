8e891066263f1f3b2f282f878b26a1d018d69386
Vidal Herrera <vidalherrera01ec@gmail.com>
2024-09-22 14:47:56 -0400

    Subiendo cambios

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/sw.js')
             .then((registration) => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, (err) => {
-                console.log('ServiceWorker registration failed: ', err);
+                console.log('Service Worker registrado con éxito:', registration);
+            })
+            .catch((error) => {
+                console.log('Error al registrar el Service Worker:', error);
             });
     });
 }
--- sw.js
+++ sw.js
@@ -1,28 +1,21 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
-];
-
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(FILES_TO_CACHE);
-            })
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/main.js',
+                '/index-7tAc9ptl.js',
+                '/index-MMG10KD8.css'
+            ]);
+        })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
